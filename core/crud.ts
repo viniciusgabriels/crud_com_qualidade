8770902f752507f00fe7ac8e8b32298a7341aeef
Vinícius Santos <viniciusgabriel.rs@hotmail.com>
2023-05-13 23:55:39 -0300

    A2.2

--- core/crud.ts
+++ core/crud.ts
@@ -2,11 +2,46 @@ import fs from "fs"; // ES6
 // const fs = require("fs"); - Como js importa
 const DB_FILE_PATH = "./core/db";
 
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
+}
+
 function create(content: string) {
+    const todo: Todo = {
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    }
+
+    const todos: Todo[] = [
+        ...read(),
+        todo,
+    ];
+
     // Precisa salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content)
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
     return content;
 }
 
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if (!db.todos) {
+        return [];
+    }
+    return db.todos;
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
 // [SIMULATION]
-console.log(create("Hoje eu preciso assistir as aulas!!"));
+CLEAR_DB();
+create("Primeira TODO");
+create("Segunda TODO");
+console.log(read());
