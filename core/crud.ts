f20a2dd218be7ecfdab055901eb8ea2a7608aea9
Vinícius Santos <viniciusgabriel.rs@hotmail.com>
2023-05-15 18:35:02 -0300

    A2.3

--- core/crud.ts
+++ core/crud.ts
@@ -1,15 +1,18 @@
 import fs from "fs"; // ES6
+import { v4 as uuid } from 'uuid';
 // const fs = require("fs"); - Como js importa
 const DB_FILE_PATH = "./core/db";
 
 interface Todo {
+    id: string;
     date: string;
     content: string;
     done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false,
@@ -24,7 +27,7 @@ function create(content: string) {
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
         todos,
     }, null, 2));
-    return content;
+    return todo;
 }
 
 function read(): Array<Todo> {
@@ -36,6 +39,33 @@ function read(): Array<Todo> {
     return db.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if (isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo);
+        }
+    });
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+
+    if (!updatedTodo) {
+        throw new Error("Couldn't update");
+    }
+
+    return updatedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo { // atalho
+    return update(id, {
+        content,
+    })
+}
+
 function CLEAR_DB() {
     fs.writeFileSync(DB_FILE_PATH, "");
 }
@@ -44,4 +74,9 @@ function CLEAR_DB() {
 CLEAR_DB();
 create("Primeira TODO");
 create("Segunda TODO");
+const terceiraTodo = create("Terceira TODO");
+// update(terceiraTodo.id, {
+//     content: "Terceira TODO com novo content"
+// });
+updateContentById(terceiraTodo.id, "Atualizada!");
 console.log(read());
