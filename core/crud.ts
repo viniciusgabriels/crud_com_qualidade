e4031d679f857f9df857cfac4205f963836a6eae
Vinícius Santos <viniciusgabriel.rs@hotmail.com>
2023-05-29 16:24:46 -0300

    a3.4 linter - editorconfig - eslintrc - prettierrc

--- core/crud.ts
+++ core/crud.ts
@@ -1,90 +1,110 @@
+/* eslint-disable no-console */
 import fs from "fs"; // ES6
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 // const fs = require("fs"); - Como js importa
 const DB_FILE_PATH = "./core/db";
 
 type UUID = string;
 
 interface Todo {
-    id: UUID;
-    date: string;
-    content: string;
-    done: boolean;
+  id: UUID;
+  date: string;
+  content: string;
+  done: boolean;
 }
 
 function create(content: string): Todo {
-    const todo: Todo = {
-        id: uuid(),
-        date: new Date().toISOString(),
-        content: content,
-        done: false,
-    }
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
 
-    const todos: Todo[] = [
-        ...read(),
-        todo,
-    ];
+  const todos: Todo[] = [...read(), todo];
 
-    // Precisa salvar o content no sistema
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+  // Precisa salvar o content no sistema
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
         todos,
-    }, null, 2));
-    return todo;
+      },
+      null,
+      2
+    )
+  );
+  return todo;
 }
 
 function read(): Array<Todo> {
-    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
-    const db = JSON.parse(dbString || "{}");
-    if (!db.todos) {
-        return [];
-    }
-    return db.todos;
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if (!db.todos) {
+    return [];
+  }
+  return db.todos;
 }
 
 function update(id: UUID, partialTodo: Partial<Todo>): Todo {
-    let updatedTodo;
-    const todos = read();
-    todos.forEach((currentTodo) => {
-        const isToUpdate = currentTodo.id === id;
-        if (isToUpdate) {
-            updatedTodo = Object.assign(currentTodo, partialTodo);
-        }
-    });
-
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos,
-    }, null, 2));
-
-    if (!updatedTodo) {
-        throw new Error("Couldn't update");
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
     }
+  });
 
-    return updatedTodo;
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+
+  if (!updatedTodo) {
+    throw new Error("Couldn't update");
+  }
+
+  return updatedTodo;
 }
 
-function updateContentById(id: UUID, content: string): Todo { // atalho
-    return update(id, {
-        content,
-    })
+function updateContentById(id: UUID, content: string): Todo {
+  // atalho
+  return update(id, {
+    content,
+  });
 }
 
 function deleteById(id: UUID) {
-    const todos = read();
-    
-    const todosWithoutOne = todos.filter((todo) => {
-        if(todo.id === id) {
-            return false;
-        }
-        return true;
-    });
+  const todos = read();
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+  const todosWithoutOne = todos.filter((todo) => {
+    if (todo.id === id) {
+      return false;
+    }
+    return true;
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
         todos: todosWithoutOne,
-    }, null, 2));
+      },
+      null,
+      2
+    )
+  );
 }
 
 function CLEAR_DB() {
-    fs.writeFileSync(DB_FILE_PATH, "");
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 // [SIMULATION]
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,10 @@
+/* eslint-disable no-console */
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
-    console.log(request.headers)
-    response.status(200).json({ message: "Olá mundo!" })
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  console.log(request.headers);
+  response.status(200).json({ message: "Olá mundo!" });
 }
